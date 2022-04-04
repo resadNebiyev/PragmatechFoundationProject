8be8eaedad717e571e234eebe04b71ae049ee548
resad nebiyev <neffresad@gmail.com>
2022-04-04 17:46:46 +0400

    tapsiriq yuklendi

---
+++ PythonWorkingWithData/apps.py
@@ -0,0 +1,54 @@
+# arr = [10,20,30,20,10,50,60,40,80,50,40]
+#1 Verilən listdəki ədədlərin kvadratlarını hesablayaraq ayrı bir listin içərisində toplayın
+
+# def my_func():
+#     cem = []
+#     for i in arr:
+#         i*=i
+#         cem += [i]
+#     print(cem)
+# my_func()
+
+#2 Verilən listin içərisəndə təkrarlanan dəyərləri silərək geri qalan deyerlerin olduğu list yaradın
+# def my_func2():
+#     new_list = []
+#     for i in arr:
+#         if i not in new_list:
+#             new_list.append(i)
+#     print(new_list)
+# my_func2()
+
+#3 Verilən listdə cüt yerdə duran ədədlərlə tək yerdə duran ədədlərin yerini dəyişdirin
+# def my_func3():
+#     new_list=[]
+#     for i in arr:
+#         new_list.append(i)
+#         new_list.reverse()
+#     print(new_list)
+# my_func3()
+
+
+# Şans oyunu yazın.
+# def my_function2():
+#     x = 0
+#     eded = []
+#     while x<3:
+#         eded.append(input("1-20 arasi eded daxil edin: "))
+#         x+=1
+#         for j in range(21):
+#             for i in eded:
+#                 if j == i           
+# my_function2()        
+#Aşağıdakı şərtləri yerinə yetirən proqram yazın
+# def my_function():
+#     usename = input("Please Enter Your Usename: ")
+#     passward = input("Please Enter Your Passward: ")
+#     if usename == "admin" and passward == "123456":
+#         print("Sistemə daxil oldunuz təşəkkür edirik")
+#     elif usename =="" or passward == "":
+#         print("Dəyərlər boş buraxila bilməz")
+#     else:
+#         print("Daxil Etdiyiniz Melumatlar Dogru Deyil")
+# my_function()
+
+
