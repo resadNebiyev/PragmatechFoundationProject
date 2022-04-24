a27aa7e30abef4f939c38be3baa50996391e19a4
resad nebiyev <neffresad@gmail.com>
2022-04-24 22:21:18 +0400

    tapsiriq yuklendi

---
+++ Project/app.py
@@ -0,0 +1,58 @@
+#1 def showFilteredNumbers(a,b):
+#     if isinstance(a, int)==False or isinstance(b, int)==False:
+#         print("Düzgün dəyər daxil edilməyib")
+#     elif a>b :
+#         for eded in range(a+1,b):
+#             if eded%5==0 and eded%7!=0:
+#                 print(eded)
+#     else:
+#         print("Daxil edilən dəyərlər tələblərə uyğun deyil" )
+# showFilteredNumbers(2,44)
+
+
+#2 def sumOfNumbers(a,b):
+#     my_list=0
+#     if isinstance(a, int)==False or isinstance(b, int)==False:
+#         print("Düzgün dəyər daxil edilməyib")
+#     else:
+#         for eded in range(a+1,b):
+#             my_list+=eded
+#         print(my_list)
+# sumOfNumbers(2,12)
+
+# def findMiddleNumber(a,b):
+#     edediorta = (a+b)/2
+#     if isinstance(a, int)==False or isinstance(b, int)==False:
+#         print("Düzgün dəyər daxil edilməyib")
+#     else:
+#         print(edediorta)
+
+
+# def ededinReqemSayiniTap(a):
+#    print(len(a))
+
+
+# def ededinReqemleriniCapEt(a):
+#     for reqem in str(a):
+#         print(reqem)
+    
+# ededinReqemleriniCapEt(2128)
+
+
+
+# def ededinReqemlerininCeminiTap(a):
+#     my_list= 0
+#     for reqem in str(a):
+#         my_list += int(reqem)
+#     print(my_list)
+    
+# ededinReqemlerininCeminiTap(123)
+
+
+# ededler=[23,56,890,12,45,123,67,23,34,12]
+# def tekralananEdedleriTap(ls):
+#     for ls in ededler:
+#         x = len(str(ls))
+#     print(x)
+# tekralananEdedleriTap(23)
+
